89d22b5fb2bcbc143ce2032c4e47027b66746099
Emmanuel <wazhingtonn84@gmail.com>
2025-05-05 15:32:28 +0300

    CRUD apis from cart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -12,21 +12,39 @@ const addToCart =async (req, res)=>{
         }
 
         await userModel.findByIdAndUpdate(req.body.userId, {cartData});
-        res.json({success: true, message: "Aded to cart succesfully!!"})
+        res.json({success: true, message: "Added to cart succesfully!!"})
     }catch(error){
         console.log(error);
-        res.json({success: false, message: "Error"})
+        res.json({success: false, message: "Error adding to cart"})
     }
 }
 
 //remove itesm from user cart
 const removeFromCart = async (req, res)=>{
-
+    try{
+        let userData= await userModel.findById(req.body.userId);
+        let cartData = await userData.cartData;
+        if(cartData[req.body.itemId]>0){
+            cartData[req.body.itemId] -= 1;
+        }
+        await userModel.findByIdAndUpdate(req.body.userId, {cartData});
+        res.json({success: true, message:"Removed from cart succesfully!!"})
+    }catch(error){
+        console.log(error);
+        res.json({success: false, message: "Error removing from cart"})
+    }
 }
 
 //fetch user cart data
 const getCart = async (req, res)=>{
-
+    try{
+        let userData = await userModel.findById(req.user.userId)
+        let cartData = await userData.cartData;
+        res.json({success: true, cartData})
+    }catch(error){
+        console.log(error)
+        res.json({success: false, message: "Error fetchindg cart"})
+    }
 }
 
 
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,21 @@
-import jwt from "jsonwebtoken"
 
-const authMiddleware = async (req, res, next)=>{
-    const {token} = req.headers;
-    if(!token){
-        return res.json({success: flase, message: "Not authorized. Login Again"})
+import jwt from "jsonwebtoken";
+
+const authMiddleware = async (req, res, next) => {
+    const { token } = req.headers;
+    if (!token) {
+        return res.status(401).json({ success: false, message: "Not authorized. Login again." });
     }
 
-    try{
-        const token_decode = jwt.verify(token, process.env.JWT_SECRET);req.body.userId = token_decode.id
+    try {
+        const token_decode = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = { userId: token_decode.id };
+
         next();
-    }catch(error){
-        console.log(error)
-        res.json({success: false, message: "Error"})
+    } catch (error) {
+        console.error("Auth error:", error);
+        res.status(403).json({ success: false, message: "Invalid or expired token." });
     }
-}
+};
 
 export default authMiddleware;
